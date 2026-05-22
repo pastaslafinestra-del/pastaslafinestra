d92c2ae1cbe3f774eaf4344f78979cb54b3fd824
moisesrojasp89-Dev <moisesrojasp.89@gmail.com>
2026-05-21 22:54:42 -0400

    nuevo cambio

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const WA_NUMBER       = '584247827899';
-export const ASSETS_BASE_URL = 'https://atjijqvwwdlydgsonptr.supabase.co/storage/v1/object/public/menu-images/';
+export const ASSETS_BASE_URL = 'https://daevefcmtmnjxliosdhf.supabase.co/storage/v1/object/public/menu-images/';
 
-export const SUPABASE_URL  = 'https://atjijqvwwdlydgsonptr.supabase.co';
-export const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0amlqcXZ3d2RseWRnc29ucHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMTI1MDIsImV4cCI6MjA5NDg4ODUwMn0.DDVEbZaVmAqmuvUs-jAHYAWm1PVUyT1zzy5gKJCdK3Q';
+export const SUPABASE_URL  = 'https://daevefcmtmnjxliosdhf.supabase.co';
+export const SUPABASE_ANON = 'sb_publishable_NjeW434fUs7h7bl8NDsczw_h1ozVnLX';
